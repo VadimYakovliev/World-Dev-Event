11dfcc76d127000858be25da96fca70864402765
Vadim Yakovliev <vadim.yakovliev@gmail.com>
2019-06-01 08:47:48 +0300

    Initial Commit

---
+++ World Dev Event/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  World Dev Event
+//
+//  Created by Vadym Yakovliev on 6/1/19.
+//  Copyright © 2019 Vadim Yakovliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ World Dev EventTests/World_Dev_EventTests.swift
@@ -0,0 +1,34 @@
+//
+//  World_Dev_EventTests.swift
+//  World Dev EventTests
+//
+//  Created by Vadym Yakovliev on 6/1/19.
+//  Copyright © 2019 Vadim Yakovliev. All rights reserved.
+//
+
+import XCTest
+@testable import World_Dev_Event
+
+class World_Dev_EventTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
